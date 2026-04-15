f24a66c53915f2fd42c4717e001f171243b59180
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-15 14:13:12 +0600

    O(n^2)

---
+++ Time Complexity/quadraticComplexity.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m; cin >> n >> m;
+
+    // O(n^2)
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "Hello" << endl;
+        }
+    }
+
+    // O(n*m)
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << "Bro" << endl;
+        }
+    }
+
+    return 0;
+}
