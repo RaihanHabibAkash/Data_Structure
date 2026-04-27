d7660e906233eb40567dc9e5dc0aadc3fafaf0f6
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-04-27 17:31:40 +0600

    Dynamic Node creation

---
+++ Linked List/Singly Linked List/dynamicNode.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val = val;
+        next = NULL;
+    }
+};
+
+int main() {
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    cout << head->val << " " << head->next->val << " " << head->next->next->val << endl;
+    cout << head->val << " " << a->val << " " << b->val << endl;
+    
+    return 0;
+}
