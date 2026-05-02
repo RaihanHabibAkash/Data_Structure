20ce5c4de4dab32cda459cac486ec4bc4e51f201
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-05-02 14:55:07 +0600

    Delete Linked list from index

---
+++ Linked List/7. More Singly Operations/deleteAtIndex.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(Node* &head, Node* &tail, int number) {
+    // Create new node
+    Node* newNode = new Node(number);
+
+    // If the linked list is empty
+    if(head == NULL) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // Connect newNode with tail
+    tail->next = newNode;
+    // newNode is tail now
+    tail = tail->next;
+}
+
+void deleteAtIndex(Node* &head, Node* &tail, int index) {
+    // Delete head
+    if(index == 1) {
+        // Pointer that will point 1st index
+        Node* deleteNode = head;
+
+        // head will point next node
+        head = head->next;
+
+        // delete delete node
+        delete deleteNode;
+        return;
+    }
+
+    // Pointer for traversing
+    Node* current = head;
+    for(int i = 1; i < index-1; i++) current = current->next;
+    // Pointer that will point the deleted index
+    Node* deleteNode = current->next;
+
+    // Delete at tail
+    if(current->next->next == NULL) {
+        // tail will point the current
+        tail = current;
+
+        // Tail next will be null
+        tail->next = NULL;
+
+        // Delete the node
+        delete deleteNode;
+        return;
+    }
+
+    // Delete at middle
+    // connecting current node with next of next node(currentNode + 2)
+    current->next = current->next->next;
+    
+    // Deleting the node
+    delete deleteNode;
+}
+
+void printing(Node* headCpy) {
+    // Base case
+    if(headCpy == NULL) {
+        cout << endl;
+        return;
+    }
+
+    cout << headCpy->val << " ";
+
+    // Main case (Recurtion)
+    printing(headCpy->next);
+}
+
+int main() {
+        // Empty linked list
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    // Taking input
+    int num;
+    while(cin >> num) insertAtTail(head, tail, num);
+
+    deleteAtIndex(head, tail, 3);
+
+    // Printing
+    printing(head);
+
+    return 0;
+}
