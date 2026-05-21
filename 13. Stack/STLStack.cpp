40596ddc0763cc9914e29a5abaf7761149ca08a7
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-05-21 12:40:15 +0600

    STL Stack

---
+++ 13. Stack/STLStack.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<int> st;
+    int val;
+    while(cin >> val) st.push(val);
+
+    while(!st.empty()) {
+        cout << st.top() <<  " ";
+        st.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
