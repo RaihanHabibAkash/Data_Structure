633844c7a59979b6e56dcfcff65b7c3359e8617c
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-05-25 03:06:02 +0600

    Min Stack

--- w14.5_Stack_Queue_Phitron/conceptSession/p155_Min_Stack.cpp
+++ w14.5_Stack_Queue_Phitron/conceptSession/p155_Min_Stack.cpp
@@ -1,3 +1,9 @@
+// Problem: Min Stack
+// Platform: LeetCode
+// Difficulty: Medium
+// Link: https://leetcode.com/problems/min-stack/
+// Topics: 2 Stack, One main other for min detection
+/*
 Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
 Implement the MinStack class:
@@ -36,3 +42,79 @@ Constraints:
 -231 <= val <= 231 - 1
 Methods pop, top and getMin operations will always be called on non-empty stacks.
 At most 3 * 104 calls will be made to push, pop, top, and getMin.
+*/
+
+
+class MinStack {
+public:
+    class Node {
+    public:
+        Node* prev;
+        int val;
+        Node* next;
+
+        Node(int val) {
+            prev = NULL;
+            this->val = val;
+            next = NULL; 
+        }
+    };
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        Node* newNd = new Node(val);
+
+        if(head == NULL) {
+            head = newNd;
+            tail = newNd;
+            return;
+        }
+
+        head->prev = newNd;
+        head->prev->next = head;
+        head = head->prev;
+    }
+    
+    void pop() {
+        if(head->next == NULL) {
+            delete head;
+            head = NULL;
+            tail = NULL;
+            return;
+        }
+
+        Node* dltNd = head;
+        head = head->next;
+        head->prev = NULL;
+        delete dltNd;
+    }
+    
+    int top() {
+        return head->val;
+    }
+    
+    int getMin() {
+        Node* cur = head->next;
+        int min = head->val;
+
+        while(cur != NULL) {
+            if(cur->val < min) min = cur->val;
+            cur = cur->next;
+        }
+        return min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
