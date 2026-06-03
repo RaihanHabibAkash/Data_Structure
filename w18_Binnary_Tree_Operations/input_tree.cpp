8e383b1d4bff37a9f6d72058bf0b3facf22cceab
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-06-03 12:27:38 +0600

    Taking input for binnary tree using queue

--- w18_Binnary_Tree_Operations/input_tree.cpp
+++ w18_Binnary_Tree_Operations/input_tree.cpp
@@ -15,41 +15,40 @@ public:
 };
 
 Bnode* input_tree() {
-    // Creating an root pointer and taking first input
     int val; cin >> val;
     Bnode* root;
 
-    // If the input value is -1 root is set to NULL, other wise root will point new node
+    // If the val is -1 don't creating node
     if(val == -1) root = NULL;
     else root = new Bnode(val);
 
-    // Queue of nodes
+    // Queue of Nodes, to maintain the order
     queue<Bnode*> q;
-    // If the root is not null pushing it
+
+    // If root is not NULL than pushing it into queue
     if(root) q.push(root);
 
     while(!q.empty()) {
-    // 1. Ber kore ana from queue
+    // 1. Ber kore nia ana from queue
         Bnode* p = q.front();
         q.pop();
-
+    
     // 2. Oi node nia kaj kora
-        // Taking new inputs
         int l, r; cin >> l >> r;
         // Creating empty pointer
         Bnode* myLeft, *myRight;
 
-        // If the input value is -1 set null, other wise create new child node 
+        // If the input is -1 placing pointer to NULL, other wise creating new node
         if(l == -1) myLeft = NULL;
         else myLeft = new Bnode(l);
         if(r == -1) myRight = NULL;
         else myRight = new Bnode(r);
 
-        // Connection
+        // Connecting with current node
         p->left = myLeft;
         p->right = myRight;
 
-    // 3. Set children to queue
+    // 3. Children thakle queue te push
         if(p->left) q.push(p->left);
         if(p->right) q.push(p->right);
     }
@@ -58,34 +57,33 @@ Bnode* input_tree() {
     return root;
 }
 
-void level_order(Bnode* root) {
+void level_order_printing(Bnode* root) {
     if(!root) {
         cout << "No Tree" << endl;
         return;
     }
-
     queue<Bnode*> q;
     q.push(root);
-
+    
     while(!q.empty()) {
-        // 1. Ber kore nia asha queue theke
+    // 1. Ber kore nia ahsa
         Bnode* f = q.front();
         q.pop();
-
-        // 2. Node dia kaj kora
+    
+    // 2. Oi node nia kaj
         cout << f->val << " ";
-
-        // 3. Node ar children push kora (thakle)
+    
+    // 3. Oi node ar children push in queue
         if(f->left) q.push(f->left);
         if(f->right) q.push(f->right);
     }
 }
 
 int main() {
+    // Taking input
     Bnode* root = input_tree();
-
     // Printing
-    level_order(root);
+    level_order_printing(root);
 
     return 0;
 }
