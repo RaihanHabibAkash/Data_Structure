11e55e688416bceb057d04ab6b007d1a58b2b252
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-06-04 15:21:45 +0600

    STL pair

--- w19_STL_pair/pair_STL.cpp
+++ w19_STL_pair/pair_STL.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main() {
     pair<string, int> p;
-    p = make_pair("Akash", 4);
+    // p = make_pair("Akash", 4);
+    p = {"Akash", 3};
     cout << p.first << " " << p.second << endl;
 
     return 0;
