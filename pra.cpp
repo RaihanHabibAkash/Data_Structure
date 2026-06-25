b863b72018f9a1e8cb2127748cfd6818c00752b2
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-06-25 06:43:12 +0600

    Heap sort practice

--- pra.cpp
+++ pra.cpp
@@ -10,43 +10,55 @@ public:
 
     void push(int val) {
         nodes.push_back(val);
-        int curIdx = nodes.size()-1;
-
-        while(curIdx > 0) {
-            int parIdx = (curIdx-1)/2;
-            if(nodes[parIdx] < nodes[curIdx]) swap(nodes[parIdx], nodes[curIdx]);
-            else break;
-
-            curIdx = parIdx;
-        }
+        up_hepify(nodes.size()-1);
     }
 
-    int pop() {
-        int mx = nodes[0];
-        nodes[0] = nodes[nodes.size()-1];
+    int pop(int idx) {
+        int mx = nodes[idx];
+        nodes[idx] = nodes[nodes.size()-1];
         nodes.pop_back();
-        int curIdx = 0;
-
-        while(nodes.size() > 1) {
-            int lIdx = curIdx*2+1,
-            rIdx = curIdx*2+2;
-            int lVal = INT_MIN, rVal = INT_MIN;
-            
-            if(lIdx < nodes.size()) lVal = nodes[lIdx];
-            if(rIdx < nodes.size()) rVal = nodes[rIdx];
-            
-            if(lVal >= rVal && lVal > nodes[curIdx]) {
-                swap(nodes[curIdx], nodes[lIdx]);
-                curIdx = lIdx;
-            }
-            else if(rVal > lVal && rVal > nodes[curIdx]) {
-                swap(nodes[curIdx], nodes[rIdx]);
-                curIdx = rIdx;
-            }
-            else break;
-        }
+        down_hepify(idx);
         return mx;
     }
+
+    void up_hepify(int idx) {
+        while(idx > 0 && nodes[idx] > nodes[(idx-1)/2]) {
+            swap(nodes[idx], nodes[(idx-1)/2]);
+            idx = (idx-1)/2;
+        }
+    }
+
+    void down_hepify(int idx) {
+        while(nodes.size() > 1) {
+            int larIdx = idx;
+            int lIdx = idx*2+1,
+                rIdx = idx*2+2;
+
+            if(lIdx < nodes.size() && nodes[lIdx] > nodes[larIdx])
+                larIdx = lIdx;
+            if(rIdx < nodes.size() && nodes[rIdx] > nodes[larIdx])
+                larIdx = rIdx;
+            
+            if(idx == larIdx) break;
+
+            swap(nodes[larIdx], nodes[idx]);
+            idx = larIdx;
+        }
+    }
+
+    vector<int> getMax() {
+        vector<int> v;
+        while(!nodes.empty()) v.push_back(pop(0));
+        return v;
+    }
+
+    void arrayToHeap(vector<int>& v) {
+        nodes = v;
+        int lastNonLeafNode = (nodes.size()/2)-1;
+
+        for(int i = lastNonLeafNode; i >= 0; i--) down_hepify(i);
+    }
+    
 
     void print() {
         for(int node : nodes) cout << node << " ";
@@ -57,15 +69,14 @@ public:
 
 int main() {
     Heap heap;
+    vector<int> arr = {1,2,3,4,5};
+    heap.arrayToHeap(arr);
+    heap.print();
     heap.push(10);
     heap.push(20);
-    heap.push(30);
-    heap.push(40);
-    heap.print();
-    cout << heap.pop() << endl;
-    cout << heap.pop() << endl;
-    heap.print();
-
+    vector<int> v = heap.getMax();
+    for(int i : v) cout << i << " ";
+    cout << endl;
 
     return 0;
 }
