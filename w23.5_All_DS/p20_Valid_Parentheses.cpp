d39284e85e53c4779d5804afd2b1a6a90320e917
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-06-28 19:54:50 +0600

    Stack LeetCode problem solve

---
+++ w23.5_All_DS/p20_Valid_Parentheses.cpp
@@ -0,0 +1,69 @@
+// Problem: Valid Parentheses
+// Platform: LeetCode
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/valid-parentheses/
+// Topics: Stack, conditions based
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+ 
+
+Constraints:
+*/
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(char c : s) {
+            if(c == '(' || c == '{' || c == '[') st.push(c);
+            else {
+                if(st.empty()) return false;
+                else if(c == ')' && st.top() != '(') return false;
+                else if(c == '}' && st.top() != '{') return false;
+                else if(c == ']' && st.top() != '[') return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
